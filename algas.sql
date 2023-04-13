4f0807182d7eb4864750fb1ce091b43535e3b94d
Eduardo Dantas de Oliveira <eduardo.oliveira@yhub.com.br>
2023-04-12 21:48:36 -0300

    fix script sql

--- algas.sql
+++ algas.sql
@@ -1,18 +1,11 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY '******';
-
-DROP USER IF EXISTS 'urubu100' @'%';
-CREATE USER IF NOT EXISTS 'urubu100' @'%' IDENTIFIED BY '******';
-GRANT ALL PRIVILEGES ON `urubu100`.* TO `urubu100` @`%`;
-FLUSH PRIVILEGES;
-
-CREATE DATABASE IF NOT EXISTS 'grupo4';
-USE 'grupo4';
+CREATE DATABASE IF NOT EXISTS grupo4;
+USE grupo4;
 
 CREATE TABLE IF NOT EXISTS `medidas` (
     id int PRIMARY KEY AUTO_INCREMENT,
     sensor VARCHAR(15),                      
     value  DOUBLE,                    
-    ingestion_date  DATE 
+    ingestion_date DATE 
 );
 
 CREATE TABLE IF NOT EXISTS `infos` (
@@ -20,6 +13,6 @@ CREATE TABLE IF NOT EXISTS `infos` (
     time_taken DOUBLE,                      
     bytes_used  DOUBLE,                    
     cpu_used  DOUBLE,
-    ram_used DOUBLE
-    ingestion_date  DATE 
+    ram_used DOUBLE,
+    ingestion_date DATE
 );
