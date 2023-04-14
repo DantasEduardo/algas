ce10f29f56aed1cd619534b46362c804db1c0099
Eduardo Dantas de Oliveira <eduardo.oliveira@yhub.com.br>
2023-04-14 02:56:44 -0300

    reorganizing python scripts

--- script_python/src/sensores/anemometro.py
+++ script_python/src/sensores/anemometro.py
@@ -6,12 +6,11 @@ class Anemometro:
         """Simulate a Anemometro, generating speed of air metrics"""
         self.speed_mean = [9.6, 15.6, 19.6]
 
-    def __generate_speed_air_mean(self) -> float:
+    def generate_speed_air_mean(self) -> float:
         """Generates speed air mean in São Paulo in the period of rain or not"""
         return self.speed_mean[random.randint(0,2)]
 
-    def simulate_speed_air(self):
+    def simulate_speed_air(self, speed_air_mean:float) -> float:
         """Simulates the speed air in São Paulo"""
-        speed_air_mean = self.__generate_speed_air_mean()
         return round(random.uniform(speed_air_mean - 5, speed_air_mean + 5), 2)
 
--- script_python/src/sensores/bmp180.py
+++ script_python/src/sensores/bmp180.py
@@ -7,23 +7,21 @@ class BPM180:
         self.pressure = [1003.3, 1013.3, 1030.3]
         self.temperature = [15.8, 17.8, 20.8]
 
-    def __generate_pressure_mean(self) -> float:
+    def generate_pressure_mean(self) -> float:
         """Generates pressure mean in São Paulo in the period of rain or not"""
         return self.pressure[random.randint(0,2)]
 
 
-    def __generate_temperature_mean(self) -> float:
+    def generate_temperature_mean(self) -> float:
         """Generates temperature mean in São Paulo in the period of rain or not"""
         return self.temperature[random.randint(0,2)]
 
 
-    def simulate_pressure(self):
+    def simulate_pressure(self, pressure_mean:float) -> float:
         """Simulates the pressure in São Paulo"""
-        pressure_mean = self.__generate_pressure_mean()
         return round(random.uniform(pressure_mean - 5, pressure_mean + 5), 2)
     
-    def simulate_temperature(self):
+    def simulate_temperature(self, temperature_mean:float) -> float:
         """Simulates the temperature in São Paulo"""
-        temperature_mean = self.__generate_temperature_mean()
         return round(random.uniform(temperature_mean - 5, temperature_mean + 5), 2)
 
