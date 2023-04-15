2349b508386d468022b1d4de0b914d19a9f19cc7
Eduardo Dantas de Oliveira <eduardo.oliveira@yhub.com.br>
2023-04-14 22:22:06 -0300

    reorganize algas.py

--- script_python/src/algas.py
+++ script_python/src/algas.py
@@ -1,4 +1,3 @@
-import argparse
 import psutil
 import random
 import time
@@ -6,21 +5,10 @@ from datetime import date
 from sys import getsizeof 
 from src.sensores.anemometro import Anemometro
 from src.sensores.bmp180 import BPM180
-from src.utils.save_data import SaveData
 
 
 QUERY_INFOS = f"INSERT INTO infos(time_taken, bytes_used, cpu_used, ram_used, ingestion_date) VALUES (%s,%s,%s,%s,%s)"
 QUERY_MEDIDAS = f"INSERT INTO medidas(sensor, value, ingestion_date) VALUES (%s,%s,%s)"
-
-
-def time_execution(fn):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = fn(*args, **kwargs)
-        end = time.time()
-        print(f'INFO: Executed in {end - start} seconds.\n\n')
-        return result
-    return wrapper
 
 def transaction_test(block, bd):
     print("Started transaction testing...")
@@ -72,64 +60,3 @@ def run(bd):
 
         count -= 1
 
-
-@time_execution
-def main(params):
-    blocks = []
-    test = False
-    if params.first:
-        test = True
-        print("Setting first test")
-        blocks.append([x for x in range(1, 1000, 100)])
-
-    if params.second:
-        test = True
-        print("Setting second test")
-        blocks.append([x for x in range(1, 1000, 10)])
-
-    if params.third:
-        test = True
-        print("Setting third test")
-        blocks.append([x for x in range(1, 1000, 1)])
-
-    if params.all:
-        test = True
-        print("Setting all tests")
-        blocks = [
-            block for block in 
-            [
-                [x for x in range(1, 1000, 100)],
-                [x for x in range(1, 1000, 10)],
-                [x for x in range(1, 1000, 1)]
-            ]
-        ]
-
-    if test:
-        mybd = SaveData()
-        for block in blocks:
-            transaction_test(block, mybd)
-
-    elif params.run:
-        run(mybd)
-        
-    else:    
-        raise Exception('Parameters not set.\nType -h to see all parameters.')    
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f','--first', default=False, action='store_true', 
-                        help='execute the first block of tests')
-    parser.add_argument('-s','--second', default=False, action='store_true', 
-                        help='execute the second block of tests')
-    parser.add_argument('-t','--third', default=False, action='store_true', 
-                        help='execute the third block of tests')
-    parser.add_argument('-a','--all', default=False, action='store_true', 
-                        help='execute the all blocks of tests')
-
-    parser.add_argument('-r','--run', default=False, action='store_true', 
-                        help='run the sensor simulator')
-
-    args = parser.parse_args()
-
-    main(args)
