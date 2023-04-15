08466fba89642ed28c5d31708d5e110435e1f710
Eduardo Dantas de Oliveira <eduardo.oliveira@yhub.com.br>
2023-04-15 20:05:32 -0300

    add se connector class

--- script_python/src/utils/db_connector.py
+++ script_python/src/utils/db_connector.py
@@ -29,3 +29,29 @@ class DBConnector:
         """Close the conection with the database"""
         self.mycursor.close()
         self.mydb.close()
+
+class S3Connection:
+    def __init__(self, bucket:str=None, path:str=None) -> None:
+        self.bucket = bucket
+        self.path_info = path+'/info/'
+        self.path_medidas = path+'/medidas/'
+        try:
+            wr.s3.does_object_exist(f"s3://{self.bucket}/{self.path}")
+        except:
+            raise Exception ("Verify you have access to this bucket or miss type the bucket or path")
+
+    def s3_upload(self, list:dict, type:str) -> None:
+        if type=="info":    
+            wr.s3.to_parquet(
+                df = pd.DataFrame(list),
+                bucket=self.bucket,
+                path=self.path_info
+            )
+        elif type=="medidas":
+            wr.s3.to_parquet(
+                df=pd.DataFrame(list),
+                bucket=self.bucket,
+                path=self.path_info
+            )
+        else:
+            raise Exception("Invalid type")
